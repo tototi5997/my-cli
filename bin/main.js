997ee1ebd3778dc5dd1d54b049ae8a3399778a90
zhangqian <zhangqian1@hyperchain.cn>
2023-12-20 19:22:50 +0800

    feat: 添加环境检测命令

--- bin/main.js
+++ bin/main.js
@@ -4,22 +4,28 @@ const chalk = require("chalk")
 const minimist = require("minimist")
 const packageJson = require("../package.json")
 
-const camelize = require("../utils/camelize")
-const cleanArgs = require("../utils/clean-args")
-
 program.version(packageJson.version).usage(`${chalk.green("<command>")} [options]`)
 
 program
   .command("create <project-name>") // <>必须输入的恶参数，[]可以选择输入的参数
-  .description("创建一个新的项目")
+  .description("create a new project")
   .option("-d, --default", "使用默认的预设")
   .option("-f, --force", "强制性覆盖创建")
+  .option("-p, --preset", "设置预设")
   .action((name, options) => {
     if (minimist(process.argv.slice(3))._.length > 1) {
       console.log(chalk.yellow("检测到您输入了多个名称，将以第一个参数作为项目名"))
     }
-    require('../lib/create')(name, options)
+    require("../lib/create")(name, options)
     // require("../lib/create")
+  })
+
+// 创建检查命令
+program
+  .command("doctor")
+  .description("check the environment")
+  .action(() => {
+    require("../lib/checkEnv")()
   })
 
 program.parse(process.argv)
