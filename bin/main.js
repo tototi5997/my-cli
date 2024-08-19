84406f0478b03ccf95805d6326ac486715c9fa04
zhangqian <zhangqian@bcds.org.cn>
2024-08-19 16:26:19 +0800

    feat: Update preset and add -c

--- bin/main.js
+++ bin/main.js
@@ -1,41 +1,41 @@
 #!/usr/bin/env node
-const { program } = require("commander")
-const chalk = require("chalk")
-const minimist = require("minimist")
-const packageJson = require("../package.json")
+const { program } = require("commander");
+const chalk = require("chalk");
+const minimist = require("minimist");
+const packageJson = require("../package.json");
 
-program.version(packageJson.version).usage(`${chalk.green("<command>")} [options]`)
+program.version(packageJson.version).usage(`${chalk.green("<command>")} [options]`);
 
 program
   .command("create <project-name>") // <>必须输入的恶参数，[]可以选择输入的参数
   .description("create a new project")
   .option("-d, --default", "使用默认的预设")
   .option("-f, --force", "强制性覆盖创建")
-  .option("-p, --preset", "设置预设")
+  .option("-p, --preset [type]", "设置预设")
   .option("-c, --clone", "使用 git clone 方式创建")
   .action((name, options) => {
     if (minimist(process.argv.slice(3))._.length > 1) {
-      console.log(chalk.yellow("检测到您输入了多个名称，将以第一个参数作为项目名"))
+      console.log(chalk.yellow("检测到您输入了多个名称，将以第一个参数作为项目名"));
     }
-    require("../lib/create")(name, options)
+    require("../lib/create")(name, options);
     // require("../lib/create")
-  })
+  });
 
 // 创建检查命令
 program
   .command("doctor")
   .description("check the environment")
   .action(() => {
-    require("../lib/checkEnv")()
-  })
+    require("../lib/checkEnv")();
+  });
 
 program
   .command("tree")
   .description("view tree")
   .option("-d, --deep", "tree deep")
   .action((_, options) => {
-    console.log(options.deep)
-    require("../utils/tree")(process.cwd(), '', 0)
-  })
+    console.log(options.deep);
+    require("../utils/tree")(process.cwd(), "", 0);
+  });
 
-program.parse(process.argv)
+program.parse(process.argv);
--- utils/load-remote-preset.js
+++ utils/load-remote-preset.js
@@ -1,27 +1,28 @@
-const fs = require("fs-extra")
+const fs = require("fs-extra");
 
 const remotePresetMap = {
-  "react-ts": "tototi5997/zli-template#main",
-}
+  "react-ts-gitlab": "gitlab:tototi5997/zli-template#main",
+  "react-ts": "github:tototi5997/zli-template#main",
+};
 
 // 远程仓库下载指定模板到临时目录
 module.exports = async function (name, targetDir, clone) {
-  const os = require("os")
-  const path = require("path")
-  const download = require("download-git-repo")
+  const os = require("os");
+  const path = require("path");
+  const download = require("download-git-repo");
 
-  const tmpdir = path.join(os.tmpdir(), "zli")
+  const tmpdir = path.join(os.tmpdir(), "zli");
 
-  await fs.remove(tmpdir)
+  await fs.remove(tmpdir);
   await new Promise((resolve, reject) => {
     download(remotePresetMap[name], tmpdir, { clone }, (err) => {
-      if (err) return reject(err)
-      resolve()
-    })
-  })
+      if (err) return reject(err);
+      resolve();
+    });
+  });
 
   return {
     targetDir,
     tmpdir,
-  }
-}
+  };
+};
